bc7cc17791afa2010a3b89ace893d73bf209859f
VolodymyrB <vladimir.babich87@gmail.com>
2022-12-30 09:24:41 +0200

    add footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,20 @@
-import { Box } from "@mui/material";
+import { AppBar, Box, Typography } from "@mui/material";
 
 export const Footer = () => {
-  return <Box>Footer</Box>;
+  return (
+    <>
+      <AppBar position="sticky" sx={{ bottom: "0", opacity: "70%" }}>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "30px",
+          }}
+        >
+          <Typography>All rights reserved 2022</Typography>
+        </Box>
+      </AppBar>
+    </>
+  );
 };
